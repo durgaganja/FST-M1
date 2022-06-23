abdb2335ae112b0619a89067af05a6f6349c8e7c
durgaganja <103400629+durgaganja@users.noreply.github.com>
2022-06-23 10:56:42 +0530

    Add files via upload

---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,14 @@
+numList = list(int(x) for x in input("Enter a sequence of comma separated values: ").split(", "))
+# numList = [10, 20, 30, 40, 10]
+print("Given list is ", numList)
+
+# Get first element in list
+firstElement = numList[0]
+# Get last element in list
+lastElement = numList[-1]
+
+# Check if first and last element are equal
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
